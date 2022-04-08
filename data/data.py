ac34b2f27c30b95053013ccca4e753adc3441bb3
Phan Gia Thong <phanthong110@gmail.com>
2022-04-08 11:18:25 +0700

    Add file code Letter_CG, Spam Email and data

--- data/data.py
+++ data/data.py
@@ -1,7 +1,7 @@
 import csv
 
-input_file=r"./spam_email.txt"
-ouput_file=r"./spam_email.csv"
+input_file=r"./letters_CG.txt"
+ouput_file=r"./letters_CG.csv"
 
 in_txt = csv.reader(open(input_file,'r'), delimiter = ' ')
 out_csv = csv.writer(open(ouput_file,'w'))
